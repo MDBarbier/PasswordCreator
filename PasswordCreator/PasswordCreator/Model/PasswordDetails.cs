bec6f8b160be7b8e1342495463a7f08e1f884315
Matt Barbier <matthewdavidbarbier@gmail.com>
2020-04-18 18:35:03 +0100

    Initial checkin of password web app

---
+++ PasswordCreator/PasswordCreator/Model/PasswordDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PasswordCreator.Model
+{
+    public class PasswordDetails
+    {
+        public string Password { get; set; }
+        public int Length { get; set; }
+        public bool IncludeSymbols { get; set; }
+    }
+}
